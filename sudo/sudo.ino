406cb3a2c039d973dea93d55be7128ce89fcbec4
scova2 <70461041+scova2@users.noreply.github.com>
2020-10-30 00:52:21 -0500

    Update sudo.ino

--- sudo/sudo.ino
+++ sudo/sudo.ino
@@ -20,17 +20,10 @@ Discription of our project.
  * Bluetooth VCC pin tO Analog  pin 5V
  * Potentioment 100K 
  * 220 resistor:
- This pins colors will be used with the wave and button switch
- * LED Green_light digital pin D13
- * LED Orange_Light digital pin D8
- * LED Red_Light digital pin D9
- * LED Blue_Light digital pin D10
- * Switch Button switch_but digital D7
- These pins colors will be used with the circular pattern.
- *LED Green_ligt2 Anolog pin A0
- *LED Orange_Light Anolog pin A1
- *LED Red_Light2 Anolog pin A2
- *LED Blue_Light2 Anolog pin A3
+ *This pins will connet a circular ring and strip colors will be used with the wave and button switch.
+ *Switch button pin to digital pin 7
+ * 220 resistor:
+ * Ground connection
 */
 
 // include the library code:
@@ -49,16 +42,8 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 //call each digital led light that will be used in wave pattern, disco pattern and button switch.
-const int Green_Light = 13; 
-const int Orange_Light = 8;
-const int Red_Light = 9;
-const int Blue_Light = 10;
 
-// call each Anolog led light that will be used in the circular pattern, disco pattern.
-const int Green_Ligt2 = A0;
-const int Orange_Light2 = A1;
-const int Red_Light2 = A2;
-const int Blue_Light2 = A3;
+\
 
 //call each pin to the bluetooth adaptor
  
@@ -79,16 +64,10 @@ void setup(){
   
   // All outputs will be setup.
   //Circular and Disco LED lights 
-  pinMode(Green_Ligt2, OUTPUT);
-  pinMode(Orange_Light2, OUTPUT);
-  pinMode(Red_Light2, OUTPUT);
-  pinMode(Blue_Light2, OUTPUT);
+
   
   //Wave, Disco and Button switch
-  pinMode(Green_Light, OUTPUT);
-  pinMode(Orange_Light,OUTPUT);
-  pinMode(Red_Light, OUTPUT);
-  pinMode(Blue_Light, OUTPUT);
+
   
   Serial.begin(9600);
 
@@ -108,103 +87,11 @@ void loop() {
 
 //Possible needed functions. 
 
-//digitalWrite(A0, HIGH); or digitalWrite(A0, LOW); works as if they are digital pins.
-//This function is used in med range voice.
-void circular_pattern(){
-  digitalWrite(Green_Light2, HIGH);
-  digitalWrite(Orange_Light2, LOW);
-  digitalWrite(Red_Light2, LOW);
-  digitalWrite(Blue_Light2, LOW);
-  delay(1000);
-  
-  digitalWrite(Green_Light2, LOW);
-  digitalWrite(Orange_Light2, HIGH);
-  digitalWrite(Red_Light2, LOW);
-  digitalWrite(Blue_Light2, LOW);
-  delay(1000);
-  
-  digitalWrite(Green_Light2, LOW);
-  digitalWrite(Orange_Light2, LOW);
-  digitalWrite(Red_Light2, HIGH);
-  digitalWrite(Blue_Light2, LOW);
-  delay(1000);
-  
-  digitalWrite(Green_Light2, LOW);
-  digitalWrite(Orange_Light2, LOW);
-  digitalWrite(Red_Light2, LOW);
-  digitalWrite(Blue_Light2, HIGH);
-  delay(1000);
-  
-  digitalWrite(Green_Light2, LOW);
-  digitalWrite(Orange_Light2, LOW);
-  digitalWrite(Red_Light2, HIGH);
-  digitalWrite(Blue_Light2, LOW);
-  delay(1000);
-
-  digitalWrite(Green_Light2, LOW);
-  digitalWrite(Orange_Light2, HIGH);
-  digitalWrite(Red_Light2, LOW);
-  digitalWrite(Blue_Light2, LOW);
-  delay(1000);
-
-  digitalWrite(Green_Light2, HIGH);
-  digitalWrite(Orange_Light2, LOW);
-  digitalWrite(Red_Light2, LOW);
-  digitalWrite(Blue_Light2, LOW);
-  delay(1000);
-
-  
-}
-void circular_pattern2(){}//optional if we use neopixal led.
-void disco_pattern(){}//similar to the wave and circular pattern just use both patterns together.
-void disco_pattern2(){}//optional if we use neopixal led.
-
-// all digital pins will be used in here. 
-//this function will be called for low range frequency of our voices.
-void wave_pattern(){
-  digitalWrite(Green_Light, HIGH);
-  digitalWrite(Orange_Light, LOW);
-  digitalWrite(Red_Light, LOW);
-  digitalWrite(Blue_Light, LOW);
-  delay(1000);
-  
-  digitalWrite(Green_Light, LOW);
-  digitalWrite(Orange_Light, HIGH);
-  digitalWrite(Red_Light, LOW);
-  digitalWrite(Blue_Light, LOW);
-  delay(1000);
-  
-  digitalWrite(Green_Light, LOW);
-  digitalWrite(Orange_Light, LOW);
-  digitalWrite(Red_Light, HIGH);
-  digitalWrite(Blue_Light, LOW);
-  delay(1000);
-  
-  digitalWrite(Green_Light, LOW);
-  digitalWrite(Orange_Light, LOW);
-  digitalWrite(Red_Light, LOW);
-  digitalWrite(Blue_Light, HIGH);
-  delay(1000);
-  
-  digitalWrite(Green_Light, LOW);
-  digitalWrite(Orange_Light, LOW);
-  digitalWrite(Red_Light, HIGH);
-  digitalWrite(Blue_Light, LOW);
-  delay(1000);
-
-  digitalWrite(Green_Light, LOW);
-  digitalWrite(Orange_Light, HIGH);
-  digitalWrite(Red_Light, LOW);
-  digitalWrite(Blue_Light, LOW);
-  delay(1000);
-
-  digitalWrite(Green_Light, HIGH);
-  digitalWrite(Orange_Light, LOW);
-  digitalWrite(Red_Light, LOW);
-  digitalWrite(Blue_Light, LOW);
-  delay(1000);
-
-}
-
-void button_switch_control(int bitbutton){}//similar to lab two. You may use the wave pattern for this function.
+void CircularWave(){}// ring rgb neopixal led.
+void DiscoWave(){}//call CirculWave and WaveCycle or create your won pattern.
+void WaveCycle(){}//rgb neopixel led light strips
+void BlinkCycle(int bitbutton){}//similar to lab two. You may use the wave pattern for this function.
 void voice_ledfrequency(int voice_recoded){} // still looking for how to do it for now work on the other three. 
+void LCDText(){}// Display text for function we having and next functikon as suggestion.
+void HandleSting(){}// to handle the string received from voice command and will convert our voice to string.
+void loop(){}// will loop 
