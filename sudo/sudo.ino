92dd52840f2e499d30b8c944a59e07aaae53ab8c
scova2 <70461041+scova2@users.noreply.github.com>
2020-11-29 12:34:38 -0600

    Update sudo.ino

--- sudo/sudo.ino
+++ sudo/sudo.ino
@@ -8,7 +8,7 @@ Sergio Covarrubias, scova2, UIN needed
 Link to a demo of our project:
 
 
-Discription of our project.
+Description of our project.
 We will have an Android emulator on Windows or Mac to connect to bluetooth with a HC - 05 bluetooth sensor module 
 to enable voice recognition. We will have a Serial port from Arduino, and perform serial readStrings to get the string from the serial port,
 and that will be our input for the project. Our input will be an input to command LEDs functionality, we will have NeoPixels and use our NeoPixels LEDs to display disco,
@@ -24,7 +24,7 @@ LEDs that you can input. We will also have a button or a switch, that you can ma
  * LCD D5 pin to Arduino digital pin 4
  * LCD D6 pin to Arduino digital pin 3
  * LCD D7 pin to Arduino digital pin 2
- * LCD R/W pin to Arduino Anolog pin GND
+ * LCD R/W pin to Arduino Analog pin GND
  * 10K resistor
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
@@ -33,24 +33,23 @@ LEDs that you can input. We will also have a button or a switch, that you can ma
  * Bluetooth TXD pin to  Arduino digital pin TX/D1
  * Bluetooth VCC pin to Arduino Analog  pin 5V
  * Bluetooth GND pin to Arduino Analog pin GND
- * Potentioment 100K 
+ * Potentiometer 100K 
  * 220 resistor:
  
- *RGB 60 LED light ground pin to Arduino Anolog GND
- *RGB 60 LED light power pin to Arduino Anolog 5v
+ *RGB 60 LED light ground pin to Arduino Analog GND
+ *RGB 60 LED light power pin to Arduino Analog 5v
  *RGB 60 LED light pin connector to Arduino digital pin 6
  *RGB 60 LED external(RED) power cable to power adaptor 5v port
  *RGB 60 LED external(WHITE) GND cable to power adaptor GND port
  
  *Switch button pin to digital pin 7
- *Switch button pin ground to Arduino Anolog GND
- *Switch button 220 resistor to Arduino Anolog 5v
+ *Switch button pin ground to Arduino Analog GND
+ *Switch button 220 resistor to Arduino Analog 5
 */
 
 // include the library code:
 #include <LiquidCrystal.h>
 #include <SoftwareSerial.h>
-#include <Adafruit_NeoPixel.h>
 #include <FastLED.h>
 
 //RGB 60 led strip.
@@ -63,49 +62,26 @@ LEDs that you can input. We will also have a button or a switch, that you can ma
 
 CRGB leds[wave_count];// used to turn on a random lights 
 
-// initialize any needed LCD interface pin, bluethooth module pin, button switch, and LED. 
+// initialize any needed LCD interface pin, bluetooth module pin, button switch, and LED. 
 // with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-// Declare our NeoPixel strip and ring objects:
-//wave
-Adafruit_NeoPixel wave_strip(wave_count, wave_pin, NEO_GRB + NEO_KHZ800);
-//what you see in the bellow is we are grouping 10 rgb led lights together and placing them 
-// all the same color. Therefore we will be needing 6 since we have 60 rgb led lights.
-uint32_t first_ten = wave_strip.Color(255,0,255);//0 through 9 or 0 through 4 color Magenta 
-uint32_t second_ten = wave_strip.Color(199,255,218);// 10 through 19 or 5 through 9. color light green
-uint32_t third_ten = wave_strip.Color(255,245,199); // 20 through 29 or 10 through 14.color cyan
-uint32_t fourth_ten = wave_strip.Color(255,255,255);// 30 through 39 or 15 through 19. color white
-uint32_t fifth_ten = wave_strip.Color(255,0,0);// 40 through 49 or 20 through 24. color red
-uint32_t six_ten = wave_strip.Color(198,100,200);//50 through 59 or 25 through 29.color light purple
-
-//second group of RGB 30 led lights Circular 
-uint32_t seven_ten = wave_strip.Color(153,255,51);//30 through 34 color lime green. 
-uint32_t eight_ten = wave_strip.Color(192,192,192);// 35 through 39 color Gray
-uint32_t nine_ten = wave_strip.Color(0,0,255); // 40 through 44. color blue
-uint32_t ten_ten = wave_strip.Color(0,0,0);// 45 through 49. color black
-uint32_t eleven_ten = wave_strip.Color(245,240,250);// 50 through 54. color #F5F0FA
-uint32_t tweleve_ten = wave_strip.Color(255,128,0);//54 through 59. color orange 
-
 //call each pin to the bluetooth adaptor 
  String store_voice_string = "";
-
  
-//used for button switch strings that will be combine and be able to print out one string in the lcd.print instead of mutiple print outs:
+//used for button switch strings that will be combine and be able to print out one string in the lcd.print instead of multiple printouts:
 String bl1 = "";
 
-
-//used for the bluetooth adaptor strings that will be combine and be able to print out one string in the lcd.print instead of mutiple print outs:
+//used for the bluetooth adaptor strings that will be combine and be able to print out one string in the lcd.print instead of multiple printouts:
 String ba1 = "";
-//ba2 and ba3 will be used for both switch button and bluetooth adaptor. 
+//ba2 and ba3 will be used for both switch button and bluetooth adapter. 
 String ba2 = "";
 String ba3 = "";
 
 //assist in shifting words on the screen to the left
 int toleft = 0;
 int aidleft = 16;
-
 //call a variable for the button switch
 const int button_switch = 7;
 int button_count = 0;
@@ -114,7 +90,7 @@ long random_number = 0;
 //this will help change the led lights to different color pallete
 CRGBPalette16 gPal;
 
-//setup has change to fit neopixal led light. 
+//setup has changed to fit a neopixel led light. 
 void setup(){
   // set up the LCD's number of columns and rows and input:
   lcd.begin(16, 2);
@@ -131,7 +107,7 @@ void setup(){
   //setting up the coloration for wave pattern and circular pattern
   FastLED.addLeds<NEOPIXEL, wave_pin>(leds, wave_count);
 
-  // All outputs will be setup.
+  // All outputs will be set up.
   
   //Wave, Disco and Button switch
   wave_strip.begin();
@@ -178,7 +154,7 @@ else if (readButton == HIGH){
 
 //Helper Functions: 
 //if the switch button selects this function then
-//let the button_count be randomly selected to choose one of the three pattern.
+//let the button_count be randomly selected to choose one of the three patterns.
 void RandomCycle(){
  ba2 = "Random selects ";
   button_count = random(3);// will select a number between 0 through 2.
@@ -199,11 +175,11 @@ void RandomCycle(){
  button_count = 0;
 }
    
-//this function is called when the switch button has been press. 
+//this function is called when the switch button has been pressed. 
 void blinkWave(){
-//depending on the button_count determines which of the four wave pattern, circular pattern, disco pattern or random selection pattern will be activated. 
+//depending on the button_count determines which of the four wave patterns, circular pattern, disco pattern or random selection pattern will be activated. 
  
- //Depending on the button_count, determines which one will be turn on. 
+ //Depending on the button_count, determines which one will be turned on. 
    if (button_count == 1){
    bl1 = " turn on Wave pattern."; 
    WaveCycle();
@@ -223,16 +199,18 @@ void blinkWave(){
 }
 
 
-// Depending on our command issued and the frequency of our voice either one of the four WaveCycle(), CiccularCycle(), DiscoCycle() or RandomCycle(), will be called. 
+// Depending on our command issued and the frequency of our voice either one of the //four WaveCycle(), CiccularCycle(), DiscoCycle() or RandomCycle(), will be called. 
 void ReceivingVocieFromPhone(String voice_recoded){
-  //this will turn on the wave pattern on. 
+  //this will turn the wave pattern on. 
   if (store_voice_string == "*turn on wave"){
     WaveCycle();
-    ba1 = " Wave pattern!";
+   
+ 	  ba1 = " Wave pattern!";
   }
-  //this will turn on the circular pattern on.
+  //this will turn the circular pattern on.
     else if(store_voice_string == "*turn on circular"){
     CircularWave();
+   
     ba1 = " Circular pattern!";
   }
   //this will activate the disco pattern.
@@ -249,7 +227,7 @@ void ReceivingVocieFromPhone(String voice_recoded){
     ba1 = " not a command try again.";
 }  
 
-//this function will create a string tha will be used in printing out a message on what was selected after our voice frequency command turns on one of the three selections. 
+//this function will create a string that will be used in printing out a message on what was selected after our voice frequency command turns on one of the three selections. 
 String comblue(){
   String combine = "";
 
@@ -263,7 +241,7 @@ String comblue(){
     return combine;
   }
 
- //this function will create a string tha will be used in printing out a message on what was selected after the button switch wae press. 
+ //this function will create a string that will be used in printing out a message on what was selected after the button switch was pressed. 
 String comswitch(){
   String combine = "";
 
@@ -278,12 +256,12 @@ String comswitch(){
   }
  
 
-//helps display message by scrolling to the left. 
+//helps display messages by scrolling to the left. 
 String move_left(String personal){
   
-  String shift = personal + "";//used to store ine the string and create an ending.
+  String shift = personal + "";//used to store in the string and create an ending.
   
-  //shifting the charcters starting from zero all the way to the end.
+  //shifting the characters starting from zero all the way to the end.
   toleft++;
   aidleft++;
   int strlength = shift.length();
@@ -293,11 +271,11 @@ String move_left(String personal){
     }
   return (shift.substring(toleft, aidleft)); 
   } 
-//----------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------
 
-//Strip activiation functions:
+//Strip activation functions:
 
-//Spliting the RGB 60 LED lights into two groups.
+//Splitting the RGB 60 LED lights into two groups.
 // code is part of Mark Kriegsman Multiple controller examples found 
 // in the FastLed library made by Daniel Garcia. 
 //the Mirroring method has been used in both the WaveCycle2() and CircularWave2().
@@ -331,7 +309,7 @@ void WaveCycle(){
         p = 0;
         }
     
-    //this for loop is foward.
+    //this for loop is forward.
     for(int t = 0; t < 30; t++) {
      // setting up our current led light
       i++;
@@ -364,10 +342,7 @@ void WaveCycle(){
   }
 }
 
-  //This function will assist to test the  last 30 neopixal lights
-  //The two  for loops will help in turning on 5 lights at one time.
-  //Look closely at leds because it increase/decrease from 5. 
-  //This means every fifth light will be the same color.
+  //This function will assist to test the  last 30 neopixel lights
   //in a circular pattern. 
   void CircularWave(){
   int j = 153;//Alters Green
@@ -396,7 +371,7 @@ void WaveCycle(){
         p = 0;
         }
     
-    //this for loop is foward.
+    //this for loop is forward.
     for(int t = 30; t < 60; t++) {
      // setting up our current led light
       i++;
@@ -407,7 +382,7 @@ void WaveCycle(){
       FastLED.show();
       delay(100);
     }
-    //this for loop is foward.
+    //this for loop is forward.
     for(int t = 30; t < 60; t++) {
      // setting up our current led light
       leds[t] = CRGB::Black;
@@ -443,7 +418,7 @@ void WaveCycle(){
     int i = 0; // the changer of lights 
     int p = 153; // holds the Green positions.
     int j = 255; // holds the Red postions.
-    int q = 51; // holds the Blue postion 
+    int q = 51; // holds the Blue positions. 
 
     
     //this while loop will change the color per iteration.
@@ -466,19 +441,18 @@ void WaveCycle(){
         p = 0;
         }
       
-    //using the for loop to light up and turn of all 60 led lights.
+    //using the for loop to light up and turn off all 60 led lights.
       for(int t = 0; t < 60; t++) {
-      // set each light to the smame pattern
+      // set each light to the same pattern
         leds[t] = CRGB(p,j,q);
       } 
       FastLED.show();
       //this for loop will turn all the lights off.
        for(int t = 0; t < 60; t++) {
-      // set each light to the smame pattern
+      // set each light to the same pattern
         leds[t] = CRGB::Black;
       }  
       i++;
       delay(1000);//delay for a second 
     }
   } 
-// --------------------------------------------------------------------------------------------
